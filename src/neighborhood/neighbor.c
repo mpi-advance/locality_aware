6d1a6a2a2cbcc567fd89664de1c401e3c0f78476
Amanda Bienz <bienz@Amandas-Work-MacBook-Pro.local>
2023-01-27 11:59:56 -0700

    Added standard MPIX_Neighbor calls (non-persistent)

--- src/neighborhood/neighbor.c
+++ src/neighborhood/neighbor.c
@@ -166,6 +166,143 @@ int init_communicationw(const void* sendbuffer,
 }
 
 
+int MPIX_Neighbor_alltoallw(
+        const void* sendbuf,
+        const int sendcounts[],
+        const MPI_Aint sdispls[],
+        MPI_Datatype* sendtypes,
+        void* recvbuf,
+        const int recvcounts[],
+        const MPI_Aint rdispls[],
+        MPI_Datatype* recvtypes,
+        MPIX_Comm* comm)
+{
+
+    MPIX_Request* request;
+    MPI_Status status;
+
+    int ierr = MPIX_Neighbor_alltoallw_init(
+            sendbuf,
+            sendcounts,
+            sdispls,
+            sendtypes,
+            recvbuf,
+            recvcounts,
+            rdispls,
+            recvtypes,
+            comm,
+            MPI_INFO_NULL,
+            &request);
+
+    MPIX_Start(request);
+    MPIX_Wait(request, &status);
+
+    return ierr;
+}
+
+int MPIX_Neighbor_alltoallv(
+        const void* sendbuffer,
+        const int sendcounts[],
+        const int sdispls[],
+        MPI_Datatype sendtype,
+        void* recvbuffer,
+        const int recvcounts[],
+        const int rdispls[],
+        MPI_Datatype recvtype,
+        MPIX_Comm* comm)
+{
+
+    MPIX_Request* request;
+    MPI_Status status;
+
+    int ierr = MPIX_Neighbor_alltoallv_init(sendbuffer,
+            sendcounts,
+            sdispls,
+            sendtype,
+            recvbuffer,
+            recvcounts,
+            rdispls,
+            recvtype,
+            comm,
+            MPI_INFO_NULL, 
+            &request);
+
+    MPIX_Start(request);
+    MPIX_Wait(request, &status);
+
+    return ierr;
+}
+
+int MPIX_Neighbor_part_locality_alltoallv(
+        const void* sendbuffer,
+        const int sendcounts[],
+        const int sdispls[],
+        MPI_Datatype sendtype,
+        void* recvbuffer,
+        const int recvcounts[],
+        const int rdispls[],
+        MPI_Datatype recvtype,
+        MPIX_Comm* comm)
+{
+
+    MPIX_Request* request;
+    MPI_Status status;
+
+    int ierr = MPIX_Neighbor_part_locality_alltoallv_init(sendbuffer,
+            sendcounts,
+            sdispls,
+            sendtype,
+            recvbuffer,
+            recvcounts,
+            rdispls,
+            recvtype,
+            comm,
+            MPI_INFO_NULL, 
+            &request);
+
+    MPIX_Start(request);
+    MPIX_Wait(request, &status);
+
+    return ierr;
+}
+
+int MPIX_Neighbor_locality_alltoallv(
+        const void* sendbuffer,
+        const int sendcounts[],
+        const int sdispls[],
+        const long global_sindices[],
+        MPI_Datatype sendtype,
+        void* recvbuffer,
+        const int recvcounts[],
+        const int rdispls[],
+        const long global_rindices[],
+        MPI_Datatype recvtype,
+        MPIX_Comm* comm)
+{
+
+    MPIX_Request* request;
+    MPI_Status status;
+
+    int ierr = MPIX_Neighbor_locality_alltoallv_init(sendbuffer,
+            sendcounts,
+            sdispls,
+            global_sindices,
+            sendtype,
+            recvbuffer,
+            recvcounts,
+            rdispls,
+            global_rindices,
+            recvtype,
+            comm,
+            MPI_INFO_NULL, 
+            &request);
+
+    MPIX_Start(request);
+    MPIX_Wait(request, &status);
+
+    return ierr;
+}
+
 // Standard Persistent Neighbor Alltoallv
 // Extension takes array of requests instead of single request
 // 'requests' must be of size indegree+outdegree!
@@ -228,6 +365,7 @@ int MPIX_Neighbor_alltoallv_init(
 
     return ierr;
 }
+
 
 
 // Standard Persistent Neighbor Alltoallv
@@ -499,7 +637,7 @@ int MPIX_Neighbor_part_locality_alltoallv_init(
     for (int i = 0; i < send_size; i++)
         global_send_indices[i] = first_send + i;
 
-    PMPI_Neighbor_alltoallv(global_send_indices, sendcounts, sdispls, MPI_LONG, 
+    MPI_Neighbor_alltoallv(global_send_indices, sendcounts, sdispls, MPI_LONG, 
             global_recv_indices, recvcounts, rdispls, MPI_LONG, comm->neighbor_comm);
 
     free(sources);
