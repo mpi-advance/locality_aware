cbb32d9ab55b9bdc0bd25e50456ea5cda11d7e93
Amanda Bienz <bienz@unm.edu>
2023-02-01 14:40:30 -0700

    added alltoallv implementations

--- src/collective/tests/test_suitesparse_alltoallv.cpp
+++ src/collective/tests/test_suitesparse_alltoallv.cpp
@@ -138,6 +138,81 @@ void test_matrix(const char* filename)
     }
 
 
+    // Test Each of the Standard (Pairwise) Implementations
+    alltoallv_pairwise(alltoallv_send_vals.data(), 
+            sendcounts.data(),
+            sdispls.data(),
+            MPI_INT,
+            mpi_recv_vals.data(),
+            recvcounts.data(),
+            rdispls.data(),
+            MPI_INT,
+            locality_comm->global_comm);
+    // 3. Compare std_recv_vals and nap_recv_vals
+    for (int i = 0; i < A.recv_comm.size_msgs; i++)
+    {
+        ASSERT_EQ(std_recv_vals[i], mpi_recv_vals[i]);
+    }
+    
+    alltoallv_pairwise_log2(alltoallv_send_vals.data(), 
+            sendcounts.data(),
+            sdispls.data(),
+            MPI_INT,
+            mpi_recv_vals.data(),
+            recvcounts.data(),
+            rdispls.data(),
+            MPI_INT,
+            locality_comm->global_comm);
+    // 3. Compare std_recv_vals and nap_recv_vals
+    for (int i = 0; i < A.recv_comm.size_msgs; i++)
+    {
+        ASSERT_EQ(std_recv_vals[i], mpi_recv_vals[i]);
+    }
+
+    alltoallv_nonblocking(alltoallv_send_vals.data(), 
+            sendcounts.data(),
+            sdispls.data(),
+            MPI_INT,
+            mpi_recv_vals.data(),
+            recvcounts.data(),
+            rdispls.data(),
+            MPI_INT,
+            locality_comm->global_comm);
+    // 3. Compare std_recv_vals and nap_recv_vals
+    for (int i = 0; i < A.recv_comm.size_msgs; i++)
+    {
+        ASSERT_EQ(std_recv_vals[i], mpi_recv_vals[i]);
+    }
+
+    alltoallv_pairwise_nonblocking(alltoallv_send_vals.data(), 
+            sendcounts.data(),
+            sdispls.data(),
+            MPI_INT,
+            mpi_recv_vals.data(),
+            recvcounts.data(),
+            rdispls.data(),
+            MPI_INT,
+            locality_comm->global_comm);
+    // 3. Compare std_recv_vals and nap_recv_vals
+    for (int i = 0; i < A.recv_comm.size_msgs; i++)
+    {
+        ASSERT_EQ(std_recv_vals[i], mpi_recv_vals[i]);
+    }
+
+    alltoallv_pairwise_nonblocking_log2(alltoallv_send_vals.data(), 
+            sendcounts.data(),
+            sdispls.data(),
+            MPI_INT,
+            mpi_recv_vals.data(),
+            recvcounts.data(),
+            rdispls.data(),
+            MPI_INT,
+            locality_comm->global_comm);
+    // 3. Compare std_recv_vals and nap_recv_vals
+    for (int i = 0; i < A.recv_comm.size_msgs; i++)
+    {
+        ASSERT_EQ(std_recv_vals[i], mpi_recv_vals[i]);
+    }
 
     MPIX_Comm_free(locality_comm);
 }
