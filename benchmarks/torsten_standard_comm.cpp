a44d523a796ff94b6c1d14735ee2205fdbf99455
ageyko <ageyko@unm.edu>
2023-02-10 10:36:32 -0700

    fixed errors

--- benchmarks/torsten_standard_comm.cpp
+++ benchmarks/torsten_standard_comm.cpp
@@ -59,9 +59,9 @@ int main(int argc, char* argv[])
         double max_time = 0;
         int max_msg_count = 0;
         int max_msg_size = 0;
-        MPI_Allreduce(&(info(0)), &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-        MPI_Allreduce(&(info(1)), &max_msg_count, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-        MPI_Allreduce(&(info(2)), &max_msg_size, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&(get<0>(info)), &max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&(get<1>(info)), &max_msg_count, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&(get<2>(info)), &max_msg_size, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
         if(rank == 0 && i == 0) 
         {
             printf("MAX_MSG_COUNT %d, MAX_MSG_SIZE %d\n");
