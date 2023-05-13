201f21b7e6fe4c1023e8ceb06807ec7cbfb8fa72
Andrew Geyko <ageyko@hopper.alliance.unm.edu>
2023-05-12 18:08:13 -0600

    merging

--- benchmarks/job_scripts/hopper/make_scripts.py
+++ benchmarks/job_scripts/hopper/make_scripts.py
@@ -3,7 +3,7 @@ import os
 matrix_names = ["D_10","bcsstk01","ch-5-b1","dwt-162","gams10a","gams10am","impcol_c","odepa400","oscil_dcop_11","tumorAntiAngiogenesis_4","west0132","ww_36_pmec_36","3elt","abb313","M40PI_n1","M80PI_n1"]
 
 # Creates batch files which run the tests using just one node
-def Create_One_Node_Test(m_name : str, algo : str, out_name : str):
+def Create_One_Node_Test(m_name : str, algo : str, out_name : str, num_tests : int):
   fp = open(f"{m_name}_{algo}_HOPPER_ONE_NODE.sh", "w")
   fp.write("#!/usr/bin/bash\n")
   fp.write(f"#SBATCH --output ../../../benchmark_tests/standard_torsten/{m_name}/{m_name}_Hopper_{out_name}_one_node\n")
@@ -16,11 +16,11 @@ def Create_One_Node_Test(m_name : str, algo : str, out_name : str):
   fp.write("#SBATCH --mail-user=ageyko@unm.edu\n\n")
   fp.write("module load openmpi\n\n")
   for k in range(2,33):
-    fp.write(f"srun --partition=general --nodes=1 --ntasks={k} --time=00:30:00 ../../../build_hopper/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm 10 {m_name} {algo}\n")
+    fp.write(f"srun --partition=general --nodes=1 --ntasks={k} --time=00:30:00 ../../../build_hopper/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm {num_tests} {m_name} {algo}\n")
   fp.close()
 
 # Creates batch files which run the tests using two nodes
-def Create_Many_Node_Test(m_name : str, algo : str, out_name : str):
+def Create_Many_Node_Test(m_name : str, algo : str, out_name : str, num_tests : int):
   fp = open(f"{m_name}_{algo}_HOPPER_MANY_NODE.sh","w")
   fp.write("#!/usr/bin/bash\n")
   fp.write(f"#SBATCH --output ../../../benchmark_tests/standard_torsten/{m_name}/{m_name}_Hopper_{out_name}_many_node\n")
@@ -33,7 +33,7 @@ def Create_Many_Node_Test(m_name : str, algo : str, out_name : str):
   fp.write("#SBATCH --mail-user=ageyko@unm.edu\n\n")
   fp.write("module load openmpi\n\n")
   for k in range(33,65):
-    fp.write(f"srun --partition=general --nodes=2 --ntasks={k} --time=00:30:00 ../../../build_hopper/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm 10 {m_name} {algo}\n")
+    fp.write(f"srun --partition=general --nodes=2 --ntasks={k} --time=00:30:00 ../../../build_hopper/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm {num_tests} {m_name} {algo}\n")
   fp.close()
 
 
@@ -42,12 +42,13 @@ for (i, m_name) in enumerate(matrix_names):
     os.mkdir(f"../../../benchmark_tests/standard_torsten/{m_name}/")
 
   # CREATE ONE NODE TEST CASES
-  Create_One_Node_Test(m_name, "STANDARD", "Standard")
-  Create_One_Node_Test(m_name, "TORSTEN", "Torsten")
-  Create_One_Node_Test(m_name, "RMA", "RMA")
-  Create_One_Node_Test(m_name, "RMA_DYNAMIC", "RMA_DYNAMIC")
+  num_tests = 10
+  Create_One_Node_Test(m_name, "STANDARD", "Standard", num_tests)
+  Create_One_Node_Test(m_name, "TORSTEN", "Torsten", num_tests)
+  Create_One_Node_Test(m_name, "RMA", "RMA", num_tests)
+  Create_One_Node_Test(m_name, "RMA_DYNAMIC", "RMA_DYNAMIC", num_tests)
 
   # CREATE MANY NODE TEST CASES
-  Create_Many_Node_Test(m_name, "STANDARD", "Standard")
-  Create_Many_Node_Test(m_name, "TORSTEN", "Torsten")
-  Create_Many_Node_Test(m_name, "RMA", "RMA")
+  Create_Many_Node_Test(m_name, "STANDARD", "Standard", num_tests)
+  Create_Many_Node_Test(m_name, "TORSTEN", "Torsten", num_tests)
+  Create_Many_Node_Test(m_name, "RMA", "RMA", num_tests)
--- benchmarks/job_scripts/wheeler/make_scripts.py
+++ benchmarks/job_scripts/wheeler/make_scripts.py
@@ -4,7 +4,7 @@ matrix_names = ["D_10","bcsstk01","ch-5-b1","dwt-162","gams10a","gams10am","impc
 
 
 # Create batch files for one node
-def Create_One_Node_Test(m_name : str, algo : str, out_name : str):
+def Create_One_Node_Test(m_name : str, algo : str, out_name : str, num_tests : int):
   fp = open(f"{m_name}_{algo}_WHEELER_ONE_NODE.sh", "w")
   fp.write("#!/usr/bin/bash\n")
   fp.write(f"#SBATCH --output ../../../benchmark_tests/standard_torsten/{m_name}/{m_name}_Wheeler_{out_name}_one_node\n")
@@ -17,14 +17,14 @@ def Create_One_Node_Test(m_name : str, algo : str, out_name : str):
   fp.write("#SBATCH --mail-user=ageyko@unm.edu\n\n")
   fp.write("module load openmpi\n\n")
   for i in range(2,9): 
-    fp.write(f"srun --partition=normal --nodes=1 --ntasks={i} --time=24:00:00 ../../../build_wheeler/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm 1 {m_name} {algo}\n")
+    fp.write(f"srun --partition=normal --nodes=1 --ntasks={i} --time=24:00:00 ../../../build_wheeler/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm {num_tests} {m_name} {algo}\n")
   for j in range(2,5):
     for i in range((j-1)*8+1, j*8+1):
-      fp.write(f"srun --partition=normal --nodes={j} --ntasks={i} --time=24:00:00 ../../../build_wheeler/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm 1 {m_name} {algo}\n")
+      fp.write(f"srun --partition=normal --nodes={j} --ntasks={i} --time=24:00:00 ../../../build_wheeler/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm {num_tests} {m_name} {algo}\n")
   fp.close()
 
 # Create batch files for many nodes
-def Create_Many_Node_Test(m_name : str, algo : str, out_name : str):
+def Create_Many_Node_Test(m_name : str, algo : str, out_name : str, num_tests : int):
   fp = open(f"{m_name}_{algo}_WHEELER_MANY_NODE.sh", "w")
   fp.write("#!/usr/bin/bash\n")
   fp.write(f"#SBATCH --output ../../../benchmark_tests/standard_torsten/{m_name}/{m_name}_Wheeler_{out_name}_many_node\n")
@@ -38,10 +38,10 @@ def Create_Many_Node_Test(m_name : str, algo : str, out_name : str):
   fp.write("module load openmpi\n\n")
   for j in range(8,17):
     for i in range((j-1)*8+1,j*8+1):
-      fp.write(f"srun --partition=normal --nodes={j} --ntasks={i} --time=01:00:00 ../../../build_wheeler/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm 1 {m_name} {algo}\n")
+      fp.write(f"srun --partition=normal --nodes={j} --ntasks={i} --time=01:00:00 ../../../build_wheeler/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm {num_tests} {m_name} {algo}\n")
   fp.close()
 
-def Create_Power_Two_Test(m_name : str, algo : str, out_name : str):
+def Create_Power_Two_Test(m_name : str, algo : str, out_name : str, num_tests : int):
   fp = open(f"{m_name}_{algo}_WHEELER_POWER_TWO.sh","w")
   fp.write("#!/usr/bin/bash\n")
   fp.write(f"#SBATCH --output ../../../benchmark_tests/standard_torsten/{m_name}/{m_name}_Wheeler_{out_name}_many_node\n")
@@ -54,7 +54,7 @@ def Create_Power_Two_Test(m_name : str, algo : str, out_name : str):
   fp.write("#SBATCH --mail-user=ageyko@unm.edu\n\n")
   fp.write("module load openmpi\n\n")
   for j in range(5):
-    fp.write(f"srun --partition=normal --nodes={2**j} --ntasks={8*(2**j)} --time=01:00:00 ../../../build_wheeler/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm 1 {m_name} {algo}\n")
+    fp.write(f"srun --partition=normal --nodes={2**j} --ntasks={8*(2**j)} --time=01:00:00 ../../../build_wheeler/benchmarks/torsten_standard_comm ../../../test_data/{m_name}.pm {num_tests} {m_name} {algo}\n")
 
 
 
@@ -62,6 +62,8 @@ for (i, m_name) in enumerate(matrix_names):
   if(not os.path.exists(f"../../../benchmark_tests/standard_torsten/{m_name}/")):
     os.mkdir(f"../../../benchmark_tests/standard_torsten/{m_name}/")
 
+
+  num_tests = 10
   # CREATE ONE NODE TEST CASES
   # Create_One_Node_Test(m_name, "STANDARD", "Standard")
   # Create_One_Node_Test(m_name, "TORSTEN", "Torsten")
@@ -71,6 +73,7 @@ for (i, m_name) in enumerate(matrix_names):
   # Create_Many_Node_Test(m_name, "TORSTEN", "Torsten")
 
   # CREATE POWER OF TWO TEST CASES
-  Create_Power_Two_Test(m_name, "STANDARD", "Standard")
-  Create_Power_Two_Test(m_name, "TORSTEN", "Torsten")
-  Create_Power_Two_Test(m_name, "RMA", "RMA")
+  Create_Power_Two_Test(m_name, "STANDARD", "Standard", num_tests)
+  Create_Power_Two_Test(m_name, "TORSTEN", "Torsten", num_tests)
+  Create_Power_Two_Test(m_name, "RMA", "RMA", num_tests)
+  Create_Power_Two_Test(m_name, "RMA_DYNAMIC", "RMA_DYNAMIC", num_tests)
