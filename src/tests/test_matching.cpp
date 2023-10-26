7759b47e01fc3651904ac99b354591a8eafa2856
WesleyJak <jwesley1@unm.edu>
2023-10-26 15:19:09 -0600

    multiple tests

--- src/tests/test_matching.cpp
+++ src/tests/test_matching.cpp
@@ -57,12 +57,8 @@ void test_matrix(const char* filename)
         recv_vals.resize(A.recv_comm.size_msgs);
     }
 
-    // TODO : Time order_comm
-    t0 = MPI_Wtime();
+    // Order recv procs for communicate correctness test
     order_comm(A, send_vals, recv_vals, MPI_INT); 
-    tfinal = MPI_Wtime() - t0;
-    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    if (rank == 0) printf("order_comm time: %e\n", t0);
 
     std::vector<int> new_recv_vals;
     if (A.recv_comm.size_msgs)
@@ -72,23 +68,74 @@ void test_matrix(const char* filename)
 
     // Time original communciate (warm-up first)
     communicate(A, send_vals, recv_vals, MPI_INT);
-
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    
     t0 = MPI_Wtime();
     for (int i = 0; i < n_iter; i++)
         communicate(A, send_vals, recv_vals, MPI_INT);
     tfinal = MPI_Wtime() - t0;
     MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    if (rank == 0) printf("Communication Time : %e\n", t0);
+    if (rank == 0) printf("%s Communication 1-1 Time: %e\n", filename, t0);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_iter; i++)
+        communicate(A, send_vals, recv_vals, MPI_INT);
+    tfinal = MPI_Wtime() - t0;
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("%s Communication 1-2 Time: %e\n", filename, t0);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_iter; i++)
+        communicate(A, send_vals, recv_vals, MPI_INT);
+    tfinal = MPI_Wtime() - t0;
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("%s Communication 1-3 Time: %e\n", filename, t0);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_iter; i++)
+        communicate(A, send_vals, recv_vals, MPI_INT);
+    tfinal = MPI_Wtime() - t0;
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("%s Communication 1-4 Time: %e\n", filename, t0);
 
     // Time new communicate (warm-up first)
     communicate2(A, send_vals, new_recv_vals, MPI_INT);
 
+    MPI_Barrier(MPI_COMM_WORLD);
     t0 = MPI_Wtime();
     for (int i = 0; i < n_iter; i++)
         communicate2(A, send_vals, new_recv_vals, MPI_INT);
     tfinal = MPI_Wtime() - t0;
     MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    if (rank == 0) printf("Communication 2 Time: %e\n", t0);
+    if (rank == 0) printf("%s Communication 2-1 Time: %e\n", filename, t0);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_iter; i++)
+        communicate2(A, send_vals, new_recv_vals, MPI_INT);
+    tfinal = MPI_Wtime() - t0;
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("%s Communication 2-2 Time: %e\n", filename, t0);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_iter; i++)
+        communicate2(A, send_vals, new_recv_vals, MPI_INT);
+    tfinal = MPI_Wtime() - t0;
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("%s Communication 2-3 Time: %e\n", filename, t0);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_iter; i++)
+        communicate2(A, send_vals, new_recv_vals, MPI_INT);
+    tfinal = MPI_Wtime() - t0;
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("%s Communication 2-4 Time: %e\n", filename, t0);
 
 
     // Check for correctness
@@ -96,6 +143,17 @@ void test_matrix(const char* filename)
     communicate2(A, send_vals, new_recv_vals, MPI_INT);
     for (int i = 0; i < A.recv_comm.size_msgs; i++)
         ASSERT_EQ(recv_vals[i], new_recv_vals[i]);
+
+    // Order comm for timing
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_iter; i++)
+    {
+        order_comm(A, send_vals, recv_vals, MPI_INT);
+    }
+    tfinal = MPI_Wtime() - t0;
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("%s Order comm time (1 order_comm): %e\n", filename, t0/n_iter);
 
     // TODO : 
     //    1. Add a new method similar to communicate, but instead of posting MPI_Irecv for each message
@@ -125,24 +183,24 @@ TEST(RandomCommTest, TestsInTests)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    //test_matrix("../../../test_data/dwt_162.pm");
-    //test_matrix("../../../test_data/odepa400.pm");
-    //test_matrix("../../../test_data/ww_36_pmec_36.pm");
-    //test_matrix("../../../test_data/bcsstk01.pm");
-    //test_matrix("../../../test_data/west0132.pm");
-    //test_matrix("../../../test_data/gams10a.pm");
-    //test_matrix("../../../test_data/gams10am.pm");
-    //test_matrix("../../../test_data/D_10.pm");
-    //test_matrix("../../../test_data/oscil_dcop_11.pm");
-    //test_matrix("../../../test_data/tumorAntiAngiogenesis_4.pm");
-    //test_matrix("../../../test_data/ch5-5-b1.pm");
-    //test_matrix("../../../test_data/msc01050.pm");
-    //test_matrix("../../../test_data/SmaGri.pm");
-    //test_matrix("../../../test_data/radfr1.pm");
-    //test_matrix("../../../test_data/bibd_49_3.pm");
-    //test_matrix("../../../test_data/can_1054.pm");
-    //test_matrix("../../../test_data/can_1072.pm");
-    //test_matrix("../../../test_data/lp_sctap2.pm");
+    test_matrix("../../../test_data/dwt_162.pm");
+    test_matrix("../../../test_data/odepa400.pm");
+    test_matrix("../../../test_data/ww_36_pmec_36.pm");
+    test_matrix("../../../test_data/bcsstk01.pm");
+    test_matrix("../../../test_data/west0132.pm");
+    test_matrix("../../../test_data/gams10a.pm");
+    test_matrix("../../../test_data/gams10am.pm");
+    test_matrix("../../../test_data/D_10.pm");
+    test_matrix("../../../test_data/oscil_dcop_11.pm");
+    test_matrix("../../../test_data/tumorAntiAngiogenesis_4.pm");
+    test_matrix("../../../test_data/ch5-5-b1.pm");
+    test_matrix("../../../test_data/msc01050.pm");
+    test_matrix("../../../test_data/SmaGri.pm");
+    test_matrix("../../../test_data/radfr1.pm");
+    test_matrix("../../../test_data/bibd_49_3.pm");
+    test_matrix("../../../test_data/can_1054.pm");
+    test_matrix("../../../test_data/can_1072.pm");
+    test_matrix("../../../test_data/lp_sctap2.pm");
     test_matrix("../../../test_data/lp_woodw.pm");
 }
 
