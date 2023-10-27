7c5989f7486cc6b736cb3bb7494e3ea8703e0887
ageyko <ageyko@unm.edu>
2023-10-27 14:25:25 +0200

    created heteregeneous benchmarks

--- benchmarks/test_comm_setup_GPU.cpp
+++ benchmarks/test_comm_setup_GPU.cpp
@@ -25,8 +25,12 @@ int main(int argc, char* argv[])
     MPI_Init(&argc, &argv);
 
     int rank, num_procs;
+    long* off_proc_cols_d;
+    int off_proc_cols_count;
+    int* send_comm_idx_d;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
 
     double t0, tfinal; 
     int n_iter = 3; 
@@ -35,8 +39,50 @@ int main(int argc, char* argv[])
     readParMatrix(filename, A);
     form_recv_comm(A);
     
-    A.recv_comm->idx
+    // Copy off proc cols to GPU
+    cudaMallocHost((void**)&off_proc_cols_d, sizeof(long)*(A.off_proc_columns.size()));
+    cudaMemcpy(off_proc_cols_d, A.off_proc_columns, sizeof(long)*A.off_proc_columns, cudaMemcpyHostToDevice);
+    off_proc_cols_count = A.off_proc_columns.size();
 
+    // Test Standard copy to cpu
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_iter; i++)
+    {
+        form_send_comm_standard_copy_to_cpu(A, off_proc_cols_d, off_proc_cols_count, send_comm_idx_d);
+        A.reset_comm();
+        cudaFreeHost(send_comm_idx_d);
+    }
+    tfinal = MPI_Wtime() - t0;
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("Standard copy to cpu: %e\n", t0);
+
+    // Test Standard gpu aware
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_iter; i++)
+    {
+        form_send_comm_standard_gpu_aware(A, off_proc_cols_d, send_comm_idx_d);
+        A.reset_comm();
+        cudaFreeHost(send_comm_idx_d);
+    }
+    tfinal = MPI_Wtime() - t0; 
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("Standard gpu aware: %e\n", t0);
+
+    // Test Torsten copy to cpu
+    MPI_Barrier(MPI_COMM_WORLD);
+    t0 = MPI_Wtime();
+    for (int i = 0; i < n_ter; i++)
+    {
+        form_send_comm_torsten_copy_to_cpu(A, off_proc_cols_d, off_proc_cols_count, send_comm_idx_d);
+        cudaFreeHost(send_comm_idx_d);
+    }
+    tfinal = MPI_Wtime() - t0;
+    MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) prtinf("Torsten copy to cpu: %e\n", t0);
+
+    cudaFreeHost(off_proc_cols_d);
     MPI_Finalize();
     return 0;
 }
