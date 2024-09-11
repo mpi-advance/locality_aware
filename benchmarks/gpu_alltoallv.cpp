ab4d33341b3b3e5fc4b870c1bcb4e5496c35affb
Evelyn Namugwanya <enamug@lassen708.coral.llnl.gov>
2024-09-11 14:36:34 -0700

    Added a strong scaling benchmark:)

--- benchmarks/gpu_alltoallv.cpp
+++ benchmarks/gpu_alltoallv.cpp
@@ -17,34 +17,35 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    int max_i = 20; // adjust to get larger sizes from here
-    int max_s = pow(2, max_i);
+    int max_i = 8; // adjust to get larger sizes from here
+    int total_size = pow(2, max_i); // Total problem size (constant)
+    int s = total_size / num_procs; // Size per process decreases as num_procs increases
     int n_iter = 100;
     double t0, tfinal;
 
     srand(time(NULL));
-    std::vector<double> send_data(max_s*num_procs);
-    std::vector<double> pmpi_alltoallv(max_s*num_procs);
-    std::vector<double> mpix_alltoallv(max_s*num_procs);
-    for (int j = 0; j < max_s*num_procs; j++)
+    std::vector<double> send_data(s*num_procs);
+    std::vector<double> pmpi_alltoallv(s*num_procs);
+    std::vector<double> mpix_alltoallv(s*num_procs);
+
+    // Initialize the data to send
+    for (int j = 0; j < s; j++) 
         send_data[j] = rand();
 
-    char* cpu_recvbuf; char* cpu_sendbuf;
+    char* cpu_recvbuf; 
+    char* cpu_sendbuf;
 
     double* send_data_d;
     double* recv_data_d;
-    cudaMalloc((void**)(&send_data_d), max_s*num_procs*sizeof(double));
-    cudaMalloc((void**)(&recv_data_d), max_s*num_procs*sizeof(double));
-    cudaMemcpy(send_data_d, send_data.data(), max_s*num_procs*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)(&send_data_d), s*num_procs*sizeof(double));
+    cudaMalloc((void**)(&recv_data_d), s*num_procs*sizeof(double));
+    cudaMemcpy(send_data_d, send_data.data(), s*num_procs*sizeof(double), cudaMemcpyHostToDevice);
 
+     
     MPIX_Comm* locality_comm;
     MPIX_Comm_init(&locality_comm, MPI_COMM_WORLD);
 
-    for (int i = 0; i < max_i; i++)
-    {
-        int s = pow(2, i);
-        
-
+    
         std::vector<int> sendcounts(num_procs);
         std::vector<int> sdispls(num_procs);
         std::vector<int> recvcounts(num_procs);
@@ -59,6 +60,8 @@ int main(int argc, char* argv[])
            // printf("Process %d, sendcounts[%d]=%d, recvcounts[%d]=%d, sdispls[%d]=%d, rdispls[%d]=%d\n",
              //      rank, j, sendcounts[j], j, recvcounts[j], j, sdispls[j], j, rdispls[j]);
         }
+    
+     
 
         int sendcount = 0;
         int recvcount = 0;
@@ -68,20 +71,25 @@ int main(int argc, char* argv[])
         sendcount += sendcounts[i];//sendcounts[i];
         recvcount += recvcounts[i];//recvcounts[i];
         }
-
+     
         // allocating memory  cpu_recvbuf, cpu_sendbuf for thecopy_to_cpu_alltoallv_pairwise_extra and copy_to_cpu_alltoallv_nonblocking_extra ;
 
         cudaMallocHost((void**)&cpu_sendbuf,  sendcount * sizeof(double));
         cudaMallocHost((void**)&cpu_recvbuf,  recvcount * sizeof(double));
 
-
+    
         MPI_Barrier(MPI_COMM_WORLD);  // Ensuring all processes reach this point
      
         // Standard MPI Implementation
+   
         PMPI_Alltoallv(send_data_d, sendcounts.data(), sdispls.data(), MPI_DOUBLE,
                        recv_data_d, recvcounts.data(), rdispls.data(), MPI_DOUBLE, MPI_COMM_WORLD);
 
-        cudaMemcpy(pmpi_alltoallv.data(), recv_data_d, s*num_procs*sizeof(double), cudaMemcpyDeviceToHost);
+                       
+       
+        cudaMemcpy(pmpi_alltoallv.data(), recv_data_d, s*num_procs*sizeof(double), cudaMemcpyDeviceToHost);//s*sizeofdoble
+
+      
         cudaMemset(recv_data_d, 0, s*num_procs*sizeof(double));
 
         //  GPU-Aware Pairwise Exchange
@@ -230,8 +238,10 @@ int main(int argc, char* argv[])
         }
         tfinal = (MPI_Wtime() - t0) / n_iter;
         MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        if (rank == 0) printf("PMPI_Alltoallv Time %e\n", t0);
-
+        if (rank == 0){
+             printf("PMPI_Alltoallv Time %e\n", t0);
+             printf("Message Size: %ld bytes\n", s * sizeof(double));
+        }
         // Time GPU-Aware Pairwise Exchange
         gpu_aware_alltoallv_pairwise(send_data_d, sendcounts.data(), sdispls.data(), MPI_DOUBLE,
                                      recv_data_d, recvcounts.data(), rdispls.data(), MPI_DOUBLE, locality_comm);
@@ -245,7 +255,10 @@ int main(int argc, char* argv[])
         }
         tfinal = (MPI_Wtime() - t0) / n_iter;
         MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        if (rank == 0) printf("GPU-Aware Pairwise Exchange Time %e\n", t0);
+        if (rank == 0){
+             printf("GPU-Aware Pairwise Exchange Time %e\n", t0);
+             printf("Message Size: %ld bytes\n", s * sizeof(double));
+        }
 
         // Time GPU-Aware Nonblocking
         gpu_aware_alltoallv_nonblocking(send_data_d, sendcounts.data(), sdispls.data(), MPI_DOUBLE,
@@ -260,7 +273,11 @@ int main(int argc, char* argv[])
         }
         tfinal = (MPI_Wtime() - t0) / n_iter;
         MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        if (rank == 0) printf("GPU-Aware Nonblocking Time %e\n", t0);
+        if (rank == 0){
+
+         printf("GPU-Aware Nonblocking Time %e\n", t0);
+         printf("Message Size: %ld bytes\n", s * sizeof(double));
+        }
 
         gpu_aware_alltoallv_waitany(send_data_d, sendcounts.data(), sdispls.data(), MPI_DOUBLE,
                                         recv_data_d, recvcounts.data(), rdispls.data(), MPI_DOUBLE, locality_comm);
@@ -275,7 +292,10 @@ int main(int argc, char* argv[])
         }
         tfinal = (MPI_Wtime() - t0) / n_iter;
         MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        if (rank == 0) printf("GPU-Aware Waitany Time %e\n", t0);
+        if (rank == 0){ 
+            printf("GPU-Aware Waitany Time %e\n", t0);
+            printf("Message Size: %ld bytes\n", s * sizeof(double));
+        }
 
 
         // Time Copy-to-CPU Pairwise Exchange
@@ -291,7 +311,10 @@ int main(int argc, char* argv[])
         }
         tfinal = (MPI_Wtime() - t0) / n_iter;
         MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        if (rank == 0) printf("Copy-to-CPU Pairwise Exchange Time %e\n", t0);
+        if (rank == 0) {
+            printf("Copy-to-CPU Pairwise Exchange Time %e\n", t0);
+            printf("Message Size: %ld bytes\n", s * sizeof(double));
+        }
 
         // Time Copy-to-CPU Nonblocking
         copy_to_cpu_alltoallv_nonblocking(send_data_d, sendcounts.data(), sdispls.data(), MPI_DOUBLE,
@@ -306,7 +329,10 @@ int main(int argc, char* argv[])
         }
         tfinal = (MPI_Wtime() - t0) / n_iter;
         MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        if (rank == 0) printf("Copy-to-CPU Nonblocking Time %e\n", t0);
+        if (rank == 0) {
+            printf("Copy-to-CPU Nonblocking Time %e\n", t0);
+            printf("Message Size: %ld bytes\n", s * sizeof(double));
+        }
 
 
         copy_to_cpu_alltoallv_pairwise_extra(send_data_d,
@@ -337,7 +363,10 @@ int main(int argc, char* argv[])
         }
         tfinal = (MPI_Wtime() - t0) / n_iter;
         MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        if (rank == 0) printf("copy_to_cpu_alltoallv_pairwise_extra Time %e\n", t0);
+        if (rank == 0){
+             printf("copy_to_cpu_alltoallv_pairwise_extra Time %e\n", t0);
+             printf("Message Size: %ld bytes\n", s * sizeof(double));
+        }
 
 
         copy_to_cpu_alltoallv_nonblocking_extra(send_data_d,
@@ -368,12 +397,15 @@ int main(int argc, char* argv[])
         }
         tfinal = (MPI_Wtime() - t0) / n_iter;
         MPI_Reduce(&tfinal, &t0, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        if (rank == 0) printf("copy_to_cpu_alltoallv_nonblocking_extra Time %e\n", t0);
+        if (rank == 0) {
+            printf("copy_to_cpu_alltoallv_nonblocking_extra Time %e\n", t0);
+            printf("Message Size: %ld bytes\n", s * sizeof(double));
+        }
 
 
 
    
-    }
+    //}
 
     MPIX_Comm_free(locality_comm);
     cudaFree(send_data_d);
@@ -385,7 +417,6 @@ int main(int argc, char* argv[])
     MPI_Finalize();
     return 0;
 }
-
 
 
 
