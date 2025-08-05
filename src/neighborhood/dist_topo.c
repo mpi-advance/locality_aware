08473540c7627c2b6fe092c60b83576c5eaccdcb
Derek Schafer <kereder6788@gmail.com>
2025-08-05 16:08:16 -0600

    Tweaked MPIX_Topo_free to not try and free arrays it didn't make when degrees are 0

--- src/neighborhood/dist_topo.c
+++ src/neighborhood/dist_topo.c
@@ -119,12 +119,19 @@ int MPIX_Topo_free(MPIX_Topo** mpix_topo_ptr)
 {
     MPIX_Topo* mpix_topo = *mpix_topo_ptr;
 
-    free(mpix_topo->sources);
-    if(mpix_topo->sourceweights != MPI_UNWEIGHTED)
-        free(mpix_topo->sourceweights);
-    free(mpix_topo->destinations);
-    if(mpix_topo->destweights != MPI_UNWEIGHTED)
-        free(mpix_topo->destweights);
+    if(mpix_topo->indegree)
+    {
+        free(mpix_topo->sources);
+        if(mpix_topo->sourceweights != MPI_UNWEIGHTED)
+            free(mpix_topo->sourceweights);
+    }
+
+    if(mpix_topo->outdegree)
+    {
+        free(mpix_topo->destinations);
+        if(mpix_topo->destweights != MPI_UNWEIGHTED)
+            free(mpix_topo->destweights);
+    }
     free(mpix_topo);
     return MPI_SUCCESS;
 }
