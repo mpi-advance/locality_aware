10e5330ebdef00c21ac93a40e0eea13dda339ae7
Shannon Kinkead <shannon.kinkead@gmail.com>
2025-09-11 22:25:06 -0600

    adding header for allgether functions

---
+++ src/collective/allgather.h
@@ -0,0 +1,58 @@
+#ifndef MPI_ADVANCE_ALLREDUCE_H
+#define MPI_ADVANCE_ALLREDUCE_H
+
+#include <mpi.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int allgather_multileader(const void* sendbuf, 
+                          int sendcount, 
+                          MPI_Datatype sendtype,
+                          void *recvbuf, 
+                          int recvcount,
+                          MPI_Datatype recvtype, 
+                          MPI_Comm comm);
+
+
+int allgather_hierarchical(const void* sendbuf,
+                           int sendcount,
+                           MPI_Datatype sendtype,
+                           void *recvbuf,
+                           int recvcount,
+                           MPI_Datatype recvtype,
+                           MPI_Comm comm);
+
+int allgather_locality_aware(const void* sendbuf,
+                             int sendcount,
+                             MPI_Datatype sendtype,
+                             void *recvbuf,
+                             int recvcount,
+                             MPI_Datatype recvtype,
+                             MPI_Comm comm);
+
+int allgather_node_aware(const void* sendbuf,
+                         int sendcount,
+                         MPI_Datatype sendtype,
+                         void* recvbuf,
+                         int recvcount,
+                         MPI_Datatype recvtype,
+                         MPI_Comm comm);
+
+int allgather_multileader_locality_aware(const void* sendbuf,
+                                         int sendcount,
+                                         MPI_Datatype sendtype,
+                                         void* recvbuf,
+                                         int recvcount,
+                                         MPI_Datatype recvtype,
+                                         MPI_Comm comm);
+
+
+#ifdef 
+__cplusplus
+}
+#endif
+
+#endif // MPI_ADVANCE_ALLREDUCE_H
