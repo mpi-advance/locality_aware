9bcdd4085e55ea542d342f6540ce70582b641567
Michael Carl Adams <madams2@dt-login01.delta.ncsa.illinois.edu>
2025-09-16 22:13:00 -0500

    4 nodes

---
+++ benchmark_tests/delta/allreduce_ipc_4nodes_map_rev.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name run_allreduce_ipc_4nodes_map_rev
+#SBATCH --output slurm-%j-%x.out
+#SBATCH --error slurm-%j-%x.err
+#SBATCH -N 4
+#SBATCH --gpus-per-node=4
+#SBATCH --exclusive
+#SBATCH --partition gpuA100x4
+#SBATCH --time 00:28:00
+#SBATCH --account=bebi-delta-gpu
+
+module load cuda/12.4.0
+module load openmpi/5.0.5+cuda
+
+cd $HOME/locality_aware/build/src/heterogeneous/tests
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+mpirun --map-by ppr:1:numa --bind-to core --rank-by slot --display-map --display-allocation --report-bindings ./allreduce_gpu r
+
+mpirun --map-by ppr:2:numa --bind-to core --rank-by slot --display-map --display-allocation --report-bindings ./allreduce_mps r
+mpirun --map-by ppr:4:numa --bind-to core --rank-by slot --display-map --display-allocation --report-bindings ./allreduce_mps r
+mpirun --map-by ppr:8:numa --bind-to core --rank-by slot --display-map --display-allocation --report-bindings ./allreduce_mps r
+mpirun --map-by ppr:16:numa --bind-to core --rank-by slot --display-map --display-allocation --report-bindings ./allreduce_mps r
