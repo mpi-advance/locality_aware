af936be369f2fe577ca166ad937d404ab8f71749
Shannon Kinkead <shannon.kinkead@gmail.com>
2025-10-09 00:23:36 -0600

    implementing ballanced bellman ford, working on balanced lloyd's aggregation

--- src/communicator/mpix_comm.c
+++ src/communicator/mpix_comm.c
@@ -1,4 +1,6 @@
 #include "mpix_comm.h"
+#include <math.h>
+#include <time.h>
 
 int MPIX_Comm_init(MPIX_Comm** xcomm_ptr, MPI_Comm global_comm)
 {
@@ -95,6 +97,90 @@ int MPIX_Comm_topo_init(MPIX_Comm* xcomm)
     return MPI_SUCCESS;
 }
 
+void balancedBellmanFord(double* adjacencyMatrix, 
+                         int* clusterMembership, 
+                         int* centerRanks, 
+                         double* distances, 
+                         int* predecessors, 
+                         int* numPredecessors, 
+                         int* clusterSizes,
+                         int tBFMax,
+                         int numProcs,
+                         int numClusters)
+{
+    int t = 0;
+    int done = 1;
+    while (t < tBFMax && !done)
+    {
+        for (int i = 0; i < numProcs; i++)
+        {
+            for (int j = 0; j < numProcs; j++)
+            {
+                int clusterISize = clusterSizes[clusterMembership[i]];
+                int clusterJSize = clusterSizes[clusterMembership[j]];
+                int swithClusters = 0;
+                if (distances[i] + adjacencyMatrix[i * numProcs + j] < distances[j])
+                    swithClusters = 1;
+                else if (distanes[i] + adjacencyMatrix[i * numProcs + j] = distances[j])
+                {
+                    if (clusterISize + 1 < clusterJSize)
+                        swithClusters = true;
+                }
+
+                if (swithClusters)
+                {
+                    clusterSizes[clusterMembership[i]] = clusterISize + 1;
+                    clusterSizes[clusterMembership[j]] = clusterJSize - 1;
+                    clusterMembership[j] = clusterMembership[i];
+                    distances[j] = distances[i] + adjacencyMatrix[i * numProcs + j];
+                    numPredecessors[i] += 1;
+                    numPredecessors[predecessors[j]] -= 1;
+                    predecessors[j] = i;
+                    done = false;
+                }
+            }
+        }
+
+        t += 1;
+    }
+}
+
+void balancedLloydClustering(double* adjancencyMatrix, int* centerRanks, int numProcs, int numClusters, int tMax, int tBFMax)
+{
+    // balanced initialization
+    int* clusterMembership = (int*) malloc(numProcs * sizeof(int));
+    double* distances = (double*) malloc(numProcs * sizeof(double));
+    int* predecessors = (int*) malloc(numProcs * sizeof(int));
+    int* numPredecessors = (int*) malloc(numProcs * sizeof(int));
+    for (int i = 0; i < numProcs; i++)
+    {
+        clusterMembership[i] = 0;
+        distances[i] = INFINITY;
+        predecessors[i] = 0;
+        numPredecessors = 0;
+    }
+
+    int* clusterSizes = (int*) malloc(numClusters * sizeof(int));
+    memset(clusterSizes, 1, numClusters);
+    srand(time(NULL));
+
+    for (int clusterIndex = 0; clusterIndex < numClusters; clusterIndex++)
+    {
+        centerRanks[clusterIndex] = rand() % numProcs;
+        int nodeIndex = clusterCenters[clusterIndex];
+        distances[nodeIndex] = 0;
+        clusterMembership[nodeIndex] = clusterIndex;
+        predecessors[nodeIndex] = nodeIndex;
+        numPredecessors[nodeIndex] = 1;
+    }    
+
+    int t = 0;
+    while (t < tMax)
+    {
+
+    }
+}
+
 int MPIX_Comm_topo_cluster_init(MPIX_Comm* xcomm)
 {
     int rank, num_procs;
@@ -148,6 +234,9 @@ int MPIX_Comm_topo_cluster_init(MPIX_Comm* xcomm)
         averageDistances[i] /= 2 * num_samples;
     }
 
+    double* adjacencyMatrix = (double*) malloc(num_procs * num_procs * sizeof(double));
+    // maybe we can parallelize Bellman-Ford, but that's a problem for the future
+    MPI_Gather(averageDistances, num_procs, MPI_DOUBLE, adjacencyMatrix, num_procs, MPI_DOUBLE, 0, comm->global_comm);
     return MPI_SUCCESS;
 }
 
