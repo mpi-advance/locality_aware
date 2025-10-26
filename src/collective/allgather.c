de8967838844b4617fc3feb9ed0dcfdf9eb3cdaf
Shannon Kinkead <shannon.kinkead@gmail.com>
2025-10-26 15:54:45 -0600

    fixing communicator used in final broadcause of multileader locality aware

--- src/collective/allgather.c
+++ src/collective/allgather.c
@@ -2,6 +2,55 @@
 #include <string.h>
 #include <math.h>
 
+int allgather_hierarchical(const void* sendbuf,
+                           int sendcount,
+                           MPI_Datatype sendtype,
+                           void *recvbuf,
+                           int recvcount,
+                           MPI_Datatype recvtype,
+                           MPIX_Comm comm)
+{
+    int rank, num_procs;
+    MPI_Comm_rank(comm.global_comm, &rank);
+    MPI_Comm_size(comm.global_comm, &num_procs);
+
+    int send_size, recv_size;
+    MPI_Type_size(sendtype, &send_size);
+    MPI_Type_size(recvtype, &recv_size);
+
+    if (comm.local_comm == MPI_COMM_NULL)
+        MPIX_Comm_topo_init(&comm);
+
+    int local_rank, ppn;
+    MPI_Comm_rank(comm.local_comm, &local_rank);
+    MPI_Comm_size(comm.local_comm, &ppn);
+
+    int n_nodes = num_procs / ppn;
+
+    char* temp_buffer = NULL;
+
+    if (local_rank == 0)
+    {
+        temp_buffer = (char*) malloc(ppn * sendcount * send_size);
+    }
+    else
+    {
+        temp_buffer = (char*) malloc(sizeof(char));
+    }
+
+    MPI_Gather(sendbuf, sendcount, sendtype, temp_buffer, sendcount, sendtype, 0, comm.local_comm);
+
+    if (local_rank == 0)
+    {
+        MPI_Allgather(temp_buffer, ppn * sendcount, sendtype, recvbuf, ppn * recvcount, recvtype, comm.group_comm);
+    }
+
+    MPI_Bcast(recvbuf, recvcount * num_procs, recvtype, 0, comm.local_comm);
+
+    free(temp_buffer);
+    return MPI_SUCCESS;
+}
+
 int allgather_multileader(const void* sendbuf, 
                           int sendcount, 
                           MPI_Datatype sendtype,
@@ -69,56 +118,62 @@ int allgather_multileader(const void* sendbuf,
     return MPI_SUCCESS;
 }
 
-
-int allgather_hierarchical(const void* sendbuf,
-                           int sendcount,
-                           MPI_Datatype sendtype,
-                           void *recvbuf,
-                           int recvcount,
-                           MPI_Datatype recvtype,
-                           MPIX_Comm comm)
+int allgather_node_aware(const void* sendbuf,
+                         int sendcount,
+                         MPI_Datatype sendtype,
+                         void* recvbuf,
+                         int recvcount,
+                         MPI_Datatype recvtype,
+                         MPIX_Comm comm)
 {
     int rank, num_procs;
     MPI_Comm_rank(comm.global_comm, &rank);
     MPI_Comm_size(comm.global_comm, &num_procs);
 
-    int send_size, recv_size;
-    MPI_Type_size(sendtype, &send_size);
-    MPI_Type_size(recvtype, &recv_size);
-
-    if (comm.local_comm == MPI_COMM_NULL)
+    if (comm.local_comm == MPI_COMM_NULL)   
         MPIX_Comm_topo_init(&comm);
 
     int local_rank, ppn;
     MPI_Comm_rank(comm.local_comm, &local_rank);
     MPI_Comm_size(comm.local_comm, &ppn);
 
+    int groupCommSize;
+    MPI_Comm_size(comm.group_comm, &groupCommSize);
+
+    char* recv_buffer = (char*) recvbuf;
+    char* send_buffer = (char*) sendbuf;
+
+    int send_size, recv_size;
+    MPI_Type_size(sendtype, &send_size);
+    MPI_Type_size(recvtype, &recv_size);
+
     int n_nodes = num_procs / ppn;
 
-    char* temp_buffer = NULL;
+    char* tmpbuf = (char*) malloc(groupCommSize * sendcount * send_size);
+    char* tmpRecvBuf = (char*) malloc(num_procs * recvcount * recv_size);
 
-    if (local_rank == 0)
+    MPI_Allgather(sendbuf, sendcount, sendtype, tmpbuf, sendcount, sendtype, comm.group_comm);
+
+    MPI_Allgather(tmpbuf, groupCommSize * sendcount, sendtype, tmpRecvBuf, groupCommSize * recvcount, recvtype, comm.local_comm);
+
+    // 3. Re-order
+    int ctr = 0;
+    for (int node = 0; node < n_nodes; node++)
     {
-        temp_buffer = (char*) malloc(ppn * sendcount * send_size);
-    }
-    else
-    {
-        temp_buffer = (char*) malloc(sizeof(char));
-    }
-
-    MPI_Gather(sendbuf, sendcount, sendtype, temp_buffer, sendcount, sendtype, 0, comm.local_comm);
-
-    if (local_rank == 0)
-    {
-        MPI_Allgather(temp_buffer, ppn * sendcount, sendtype, recvbuf, ppn * recvcount, recvtype, comm.group_comm);
+        int node_offset = node * recvcount * recv_size;
+        for (int dest = 0; dest < ppn; dest++)
+        {
+            int dest_offset = dest * n_nodes * recvcount * recv_size;
+            memcpy(&(recv_buffer[ctr]), &(tmpRecvBuf[node_offset + dest_offset]), recvcount * recv_size);
+            ctr += recvcount * recv_size;
+        } 
     }
 
-    MPI_Bcast(recvbuf, recvcount * num_procs, recvtype, 0, comm.local_comm);
+    free(tmpbuf);
+    free(tmpRecvBuf);
 
-    free(temp_buffer);
     return MPI_SUCCESS;
-}
-        
+}         
 
 int allgather_locality_aware(const void* sendbuf,
                              int sendcount,
@@ -189,63 +244,6 @@ int allgather_locality_aware(const void* sendbuf,
     free(tmpRecvBuf);
 
     return MPI_ERR_OP;
-}
-
-int allgather_node_aware(const void* sendbuf,
-                         int sendcount,
-                         MPI_Datatype sendtype,
-                         void* recvbuf,
-                         int recvcount,
-                         MPI_Datatype recvtype,
-                         MPIX_Comm comm)
-{
-    int rank, num_procs;
-    MPI_Comm_rank(comm.global_comm, &rank);
-    MPI_Comm_size(comm.global_comm, &num_procs);
-
-    if (comm.local_comm == MPI_COMM_NULL)   
-        MPIX_Comm_topo_init(&comm);
-
-    int local_rank, ppn;
-    MPI_Comm_rank(comm.local_comm, &local_rank);
-    MPI_Comm_size(comm.local_comm, &ppn);
-
-    int groupCommSize;
-    MPI_Comm_size(comm.group_comm, &groupCommSize);
-
-    char* recv_buffer = (char*) recvbuf;
-    char* send_buffer = (char*) sendbuf;
-
-    int send_size, recv_size;
-    MPI_Type_size(sendtype, &send_size);
-    MPI_Type_size(recvtype, &recv_size);
-
-    int n_nodes = num_procs / ppn;
-
-    char* tmpbuf = (char*) malloc(groupCommSize * sendcount * send_size);
-    char* tmpRecvBuf = (char*) malloc(num_procs * recvcount * recv_size);
-
-    MPI_Allgather(sendbuf, sendcount, sendtype, tmpbuf, sendcount, sendtype, comm.group_comm);
-
-    MPI_Allgather(tmpbuf, groupCommSize * sendcount, sendtype, tmpRecvBuf, groupCommSize * recvcount, recvtype, comm.local_comm);
-
-    // 3. Re-order
-    int ctr = 0;
-    for (int node = 0; node < n_nodes; node++)
-    {
-        int node_offset = node * recvcount * recv_size;
-        for (int dest = 0; dest < ppn; dest++)
-        {
-            int dest_offset = dest * n_nodes * recvcount * recv_size;
-            memcpy(&(recv_buffer[ctr]), &(tmpRecvBuf[node_offset + dest_offset]), recvcount * recv_size);
-            ctr += recvcount * recv_size;
-        } 
-    }
-
-    free(tmpbuf);
-    free(tmpRecvBuf);
-
-    return MPI_SUCCESS;
 }
 
 int allgather_multileader_locality_aware(const void* sendbuf,
@@ -334,7 +332,7 @@ int allgather_multileader_locality_aware(const void* sendbuf,
     }
 
     // 3. Broadcast 
-    MPI_Bcast(recvbuf, recvcount * num_procs, recvtype, 0, comm.local_comm);
+    MPI_Bcast(recvbuf, recvcount * num_procs, recvtype, 0, comm.leader_comm);
 
     free(local_send_buffer);
     free(local_recv_buffer);
