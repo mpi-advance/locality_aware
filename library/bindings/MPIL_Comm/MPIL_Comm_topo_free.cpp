aa973ea48f4fefe22029ed1fc2bbe587daa788b5
Derek Schafer <schaferderek96@gmail.com>
2026-03-02 07:17:42 -0700

    More minor free cleanup

--- library/bindings/MPIL_Comm/MPIL_Comm_topo_free.cpp
+++ library/bindings/MPIL_Comm/MPIL_Comm_topo_free.cpp
@@ -11,7 +11,7 @@ int MPIL_Comm_topo_free(MPIL_Comm* xcomm)
     {
         return MPI_SUCCESS;
     }
-    
+
     if (xcomm->local_comm != MPI_COMM_NULL)
     {
         MPI_Comm_free(&(xcomm->local_comm));
@@ -23,21 +23,14 @@ int MPIL_Comm_topo_free(MPIL_Comm* xcomm)
         xcomm->group_comm = MPI_COMM_NULL;
     }
 
-    if (xcomm->global_rank_to_local != NULL)
-    {
-        free(xcomm->global_rank_to_local);
-        xcomm->global_rank_to_local = NULL;
-    }
-    if (xcomm->global_rank_to_node != NULL)
-    {
-        free(xcomm->global_rank_to_node);
-        xcomm->global_rank_to_node = NULL;
-    }
-    if (xcomm->ordered_global_ranks != NULL)
-    {
-        free(xcomm->ordered_global_ranks);
-        xcomm->ordered_global_ranks = NULL;
-    }
+    free(xcomm->global_rank_to_local);
+    xcomm->global_rank_to_local = NULL;
+
+    free(xcomm->global_rank_to_node);
+    xcomm->global_rank_to_node = NULL;
+
+    free(xcomm->ordered_global_ranks);
+    xcomm->ordered_global_ranks = NULL;
 
     return MPI_SUCCESS;
 }
