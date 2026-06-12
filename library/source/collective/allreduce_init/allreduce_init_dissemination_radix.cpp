0dc3eb23d1326ad312b91810b0dbc925e7f29a01
Amanda Bienz <bienz@unm.edu>
2026-06-12 14:57:20 -0600

    Passing all CPU tests

--- library/source/collective/allreduce_init/allreduce_init_dissemination_radix.cpp
+++ library/source/collective/allreduce_init/allreduce_init_dissemination_radix.cpp
@@ -57,6 +57,7 @@ int allreduce_init_dissemination_radix(const void* sendbuf,
     request->op = op;
     request->datatype = datatype;
     request->sendbuf = sendbuf;
+    request->recvbuf = recvbuf;
     MPI_Comm_dup(comm->global_comm, &(request->global_comm));
     MPIL_Alloc(&(request->tmpbuf), radix*type_size*count*num_procs);
 
