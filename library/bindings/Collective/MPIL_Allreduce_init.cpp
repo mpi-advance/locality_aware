4910a96d165d1a94bb8e217fd3fb521a1de4e43f
Amanda Bienz <bienz2@users.noreply.github.com>
2026-07-06 12:59:12 -0600

    Update library/bindings/Collective/MPIL_Allreduce_init.cpp
    Co-authored-by: Derek Schafer <schaferderek96@gmail.com>

--- library/bindings/Collective/MPIL_Allreduce_init.cpp
+++ library/bindings/Collective/MPIL_Allreduce_init.cpp
@@ -19,8 +19,10 @@ int MPIL_Allreduce_init(const void* sendbuf,
     }
 
     allreduce_init_ftn method;
-    bool gpu_aware = false;
+#if defined(GPU)
+    bool gpu_aware   = false;
     bool copy_to_cpu = false;
+#endif
 
     switch (mpil_allreduce_init_implementation)
     {
