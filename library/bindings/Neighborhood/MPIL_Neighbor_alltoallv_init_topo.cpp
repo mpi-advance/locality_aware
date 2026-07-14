085ebc8f3eeb9366dc2da5efc2bdd5421970275a
Amanda Bienz <bienz2@users.noreply.github.com>
2026-07-14 14:27:20 -0600

    Apply suggestion from @TheMasterDirk
    Co-authored-by: Derek Schafer <schaferderek96@gmail.com>

--- library/bindings/Neighborhood/MPIL_Neighbor_alltoallv_init_topo.cpp
+++ library/bindings/Neighborhood/MPIL_Neighbor_alltoallv_init_topo.cpp
@@ -19,8 +19,10 @@ int MPIL_Neighbor_alltoallv_init_topo(const void* sendbuf,
                                       MPIL_Request** request_ptr)
 {
     neighbor_alltoallv_init_ftn method;
-    bool gpu_aware = false;
+#if defined(GPU)
+    bool gpu_aware   = false;
     bool copy_to_cpu = false;
+#endif
 
     switch (mpil_neighbor_alltoallv_init_implementation)
     {
