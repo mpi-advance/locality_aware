f99e88917efc4203907dd75dd47902680143b2f3
Amanda Bienz <bienz@unm.edu>
2026-08-05 14:32:19 -0600

    Added missing file

---
+++ library/source/collective/alltoallv_init/alltoallv_init_rma.cpp
@@ -0,0 +1,118 @@
+#include "collective/alltoall_init.h"
+#include "locality_aware.h"
+
+// NOTE: No GPU-Aware Version, but Copy To CPU would work
+int alltoallv_init_rma(const void* sendbuf,
+                       const int sendcounts[],
+                       const int sdispls[],
+                       MPI_Datatype sendtype,
+                       void* recvbuf,
+                       const int recvcounts[],
+                       const int rdispls[],
+                       MPI_Datatype recvtype,
+                       MPIL_Comm* comm,
+                       MPIL_Info* info,
+                       MPIL_Request** req_ptr)
+{
+    int rank, num_procs;
+    MPI_Comm_rank(comm->global_comm, &rank);
+    MPI_Comm_size(comm->global_comm, &num_procs);
+
+    MPIL_Request* request;
+    init_request(&request);
+    
+    request->start_function = alltoall_rma_start;
+    request->wait_function = alltoall_rma_wait;
+
+    int send_bytes, recv_bytes;
+    MPI_Type_size(sendtype, &send_bytes);
+    MPI_Type_size(recvtype, &recv_bytes);
+
+    request->sendbuf = sendbuf;
+    request->recvbuf = recvbuf;
+    request->n_puts = num_procs;
+    request->sdispls = (int*)malloc(num_procs*sizeof(int));
+    request->put_displs = (int*)malloc(num_procs*sizeof(int));
+    request->put_bytes = (int*)malloc(num_procs*sizeof(int));
+
+    int bytes = 0;
+    for (int i = 0; i < num_procs; i++)
+        bytes += (recvcounts[i] * recv_bytes);
+    MPIL_Request_win_init(request, recvbuf, bytes, 1, comm->global_comm);
+
+    for (int i = 0; i < num_procs; i++)
+    {
+        request->sdispls[i] = sdispls[i];
+        request->put_bytes[i] = sendcounts[i] * send_bytes;
+    }
+    MPIL_Alltoall(rdispls, 1, MPI_INT, request->put_displs, 1, MPI_INT, comm);
+
+    *req_ptr = request;
+
+    return MPI_SUCCESS;
+
+}
+
+
+int alltoallv_rma_start(MPIL_Request* request)
+{
+#if defined(GPU)
+if (request->gpu_sendbuf)
+{
+#if defined(APU)
+    memcpy(request->tmp_gpubuf, request->gpu_sendbuf, 
+            request->n_puts * request->send_size);
+#else
+    int gpu_error;
+    gpu_error = gpuMemcpyAsync(request->tmp_gpubuf, request->gpu_sendbuf, 
+            request->n_puts * request->send_size, gpuMemcpyDeviceToHost, 0);
+    gpu_check(gpu_error);
+    gpu_error = gpuStreamSynchronize(0);
+    gpu_check(gpu_error);
+#endif
+}
+#endif
+    
+    const char*  send_buffer = (const char* )(request->sendbuf);
+
+    MPI_Win_fence(MPI_MODE_NOPRECEDE, request->win);
+
+    for (int i = 0; i < request->n_puts; ++i) {
+        MPI_Put(send_buffer + request->sdispls[i],
+                request->put_bytes[i],
+                MPI_BYTE,
+                i,
+                request->put_displs[i],
+                request->put_bytes[i],
+                MPI_BYTE,
+                request->win);
+    }
+
+    return MPI_SUCCESS;
+}
+
+
+int alltoallv_rma_wait(MPIL_Request* request, MPI_Status* status)
+{
+    MPI_Win_fence(MPI_MODE_NOSUCCEED, request->win);
+
+#if defined(GPU)
+if (request->gpu_recvbuf)
+{
+#if defined(APU)
+    memcpy(request->gpu_recvbuf, request->win_array, request->win_bytes);
+#else
+    int gpu_error;
+    gpu_error = gpuMemcpyAsync(request->gpu_recvbuf, request->win_array, 
+            request->win_bytes, gpuMemcpyHostToDevice, 0);
+    gpu_check(gpu_error);
+    gpu_error = gpuStreamSynchronize(0);
+    gpu_check(gpu_error);
+#endif
+}
+#endif
+
+
+    return MPI_SUCCESS;
+}
+    
---
+++ library/tests/test_suitesparse_alltoallv_init.cpp
@@ -0,0 +1,248 @@
+#include <assert.h>
+#include <math.h>
+#include <mpi.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <vector>
+
+#include "locality_aware.h"
+#include "tests/par_binary_IO.hpp"
+#include "tests/sparse_mat.hpp"
+
+void compare_alltoallv_results(std::vector<int>& pmpi, std::vector<int>& mpil, int s)
+{
+    for (int i = 0; i < s; i++)
+    {
+        if (pmpi[i] != mpil[i])
+        {
+            fprintf(stderr,
+                    "MPIL Alltoallv != PMPI, position %d, pmpi %d, mpil %d\n",
+                    i,
+                    pmpi[i],
+                    mpil[i]);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+    }
+}
+
+void test_matrix(const char* filename)
+{
+    int rank, num_procs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    MPIL_Comm* xcomm;
+    MPIL_Comm_init(&xcomm, MPI_COMM_WORLD);
+    MPIL_Comm_update_locality(xcomm, 4);
+
+    MPIL_Info* mpil_info;
+    MPIL_Info_init(&mpil_info);
+
+    MPIL_Request* mpil_request;
+
+    // Read suitesparse matrix
+    ParMat<int> A;
+    readParMatrix(filename, A);
+    form_comm(A);
+
+    std::vector<int> send_vals(A.on_proc.n_rows);
+    std::iota(send_vals.begin(), send_vals.end(), 0);
+    for (int i = 0; i < A.on_proc.n_rows; i++)
+    {
+        send_vals[i] += (rank * 1000);
+    }
+
+    // Alltoallv_send_vals must be ordered (dest 0 to num_procs-1)
+    std::vector<int> proc_pos(num_procs, -1);
+    for (int i = 0; i < A.send_comm.n_msgs; i++)
+    {
+        proc_pos[A.send_comm.procs[i]] = i;
+    }
+
+    std::vector<int> alltoallv_send_vals(A.send_comm.size_msgs);
+    int start, end, idx;
+    int ctr = 0;
+    for (int i = 0; i < num_procs; i++)
+    {
+        idx = proc_pos[i];
+        if (proc_pos[i] < 0)
+        {
+            continue;
+        }
+
+        start = A.send_comm.ptr[idx];
+        end   = A.send_comm.ptr[idx + 1];
+        for (int j = start; j < end; j++)
+        {
+            alltoallv_send_vals[ctr++] = send_vals[A.send_comm.idx[j]];
+        }
+    }
+
+    std::vector<int> sendcounts(num_procs, 0);
+    std::vector<int> sdispls(num_procs + 1);
+    std::vector<int> recvcounts(num_procs, 0);
+    std::vector<int> rdispls(num_procs + 1);
+
+    for (int i = 0; i < A.send_comm.n_msgs; i++)
+    {
+        sendcounts[A.send_comm.procs[i]] = A.send_comm.ptr[i + 1] - A.send_comm.ptr[i];
+    }
+    for (int i = 0; i < A.recv_comm.n_msgs; i++)
+    {
+        recvcounts[A.recv_comm.procs[i]] = A.recv_comm.ptr[i + 1] - A.recv_comm.ptr[i];
+    }
+
+    sdispls[0] = 0;
+    rdispls[0] = 0;
+    for (int i = 0; i < num_procs; i++)
+    {
+        sdispls[i + 1] = sdispls[i] + sendcounts[i];
+        rdispls[i + 1] = rdispls[i] + recvcounts[i];
+    }
+
+    std::vector<int> pmpi_recv_vals(A.recv_comm.size_msgs);
+    std::vector<int> mpil_recv_vals(A.recv_comm.size_msgs);
+
+    communicate(A, send_vals, mpil_recv_vals, MPI_INT);
+
+    PMPI_Alltoallv(alltoallv_send_vals.data(),
+                   sendcounts.data(),
+                   sdispls.data(),
+                   MPI_INT,
+                   pmpi_recv_vals.data(),
+                   recvcounts.data(),
+                   rdispls.data(),
+                   MPI_INT,
+                   MPI_COMM_WORLD);
+    compare_alltoallv_results(pmpi_recv_vals, mpil_recv_vals, A.recv_comm.size_msgs);
+
+    std::fill(mpil_recv_vals.begin(), mpil_recv_vals.end(), 0);
+    MPIL_Alltoallv(alltoallv_send_vals.data(),
+                   sendcounts.data(),
+                   sdispls.data(),
+                   MPI_INT,
+                   mpil_recv_vals.data(),
+                   recvcounts.data(),
+                   rdispls.data(),
+                   MPI_INT,
+                   xcomm);
+    compare_alltoallv_results(pmpi_recv_vals, mpil_recv_vals, A.recv_comm.size_msgs);
+
+    // Persistent MPIL Alltoallv - Default
+    std::fill(mpil_alltoallv.begin(), mpil_alltoallv.end(), 0);
+    MPIL_Alltoallv_init(alltoallv_send_vals.data(),
+                   sendcounts.data(),
+                   sdispls.data(),
+                   MPI_INT,
+                   mpil_recv_vals.data(),
+                   recvcounts.data(),
+                   rdispls.data(),
+                   MPI_INT,
+                   xcomm,
+                   mpil_info,
+                   &mpil_request);
+    MPIL_Start(mpil_request);
+    MPIL_Wait(mpil_request, MPI_STATUS_IGNORE);
+    MPIL_Request_free(&mpil_request);
+    compare_alltoallv_results(pmpi_recv_vals, mpil_recv_vals, s);
+
+
+    // Persistent MPIL Alltoallv - Pairwise
+    std::fill(mpil_alltoallv.begin(), mpil_alltoallv.end(), 0);
+    MPIL_Set_alltoallv_init_algorithm(ALLTOALLV_INIT_PAIRWISE);    
+    MPIL_Alltoallv_init(alltoallv_send_vals.data(),
+                   sendcounts.data(),
+                   sdispls.data(),
+                   MPI_INT,
+                   mpil_recv_vals.data(),
+                   recvcounts.data(),
+                   rdispls.data(),
+                   MPI_INT,
+                   xcomm,
+                   mpil_info,
+                   &mpil_request);
+    MPIL_Start(mpil_request);
+    MPIL_Wait(mpil_request, MPI_STATUS_IGNORE);
+    MPIL_Request_free(&mpil_request);
+    compare_alltoallv_results(pmpi_recv_vals, mpil_recv_vals, s);
+
+    // Persistent MPIL Alltoallv - Nonblocking 
+    std::fill(mpil_alltoallv.begin(), mpil_alltoallv.end(), 0);
+    MPIL_Set_alltoallv_init_algorithm(ALLTOALLV_INIT_NONBLOCKING);    
+    MPIL_Alltoallv_init(alltoallv_send_vals.data(),
+                   sendcounts.data(),
+                   sdispls.data(),
+                   MPI_INT,
+                   mpil_recv_vals.data(),
+                   recvcounts.data(),
+                   rdispls.data(),
+                   MPI_INT,
+                   xcomm,
+                   mpil_info,
+                   &mpil_request);
+    MPIL_Start(mpil_request);
+    MPIL_Wait(mpil_request, MPI_STATUS_IGNORE);
+    MPIL_Request_free(&mpil_request);
+    compare_alltoallv_results(pmpi_recv_vals, mpil_recv_vals, s);
+
+    // Persistent MPIL Alltoallv - RMA
+    std::fill(mpil_alltoallv.begin(), mpil_alltoallv.end(), 0);
+    MPIL_Set_alltoallv_init_algorithm(ALLTOALLV_INIT_RMA);    
+    MPIL_Alltoallv_init(alltoallv_send_vals.data(),
+                   sendcounts.data(),
+                   sdispls.data(),
+                   MPI_INT,
+                   mpil_recv_vals.data(),
+                   recvcounts.data(),
+                   rdispls.data(),
+                   MPI_INT,
+                   xcomm,
+                   mpil_info,
+                   &mpil_request);
+    MPIL_Start(mpil_request);
+    MPIL_Wait(mpil_request, MPI_STATUS_IGNORE);
+    MPIL_Request_free(&mpil_request);
+    compare_alltoallv_results(pmpi_recv_vals, mpil_recv_vals, s);
+
+
+#if defined(MPI4)
+    // Persistent MPIL Alltoallv - PMPI
+    std::fill(mpil_alltoallv.begin(), mpil_alltoallv.end(), 0);
+    MPIL_Set_alltoallv_init_algorithm(ALLTOALLV_INIT_PMPI);    
+    MPIL_Alltoallv_init(alltoallv_send_vals.data(),
+                   sendcounts.data(),
+                   sdispls.data(),
+                   MPI_INT,
+                   mpil_recv_vals.data(),
+                   recvcounts.data(),
+                   rdispls.data(),
+                   MPI_INT,
+                   xcomm,
+                   mpil_info,
+                   &mpil_request);
+    MPIL_Start(mpil_request);
+    MPIL_Wait(mpil_request, MPI_STATUS_IGNORE);
+    MPIL_Request_free(&mpil_request);
+    compare_alltoallv_results(pmpi_recv_vals, mpil_recv_vals, s);
+#endif
+
+
+    MPIL_Comm_free(&xcomm);
+}
+
+int main(int argc, char** argv)
+{
+    MPI_Init(&argc, &argv);
+    MPIL_Init(MPI_COMM_WORLD);
+
+    test_matrix("../../../test_data/dwt_162.pm");
+    test_matrix("../../../test_data/odepa400.pm");
+    test_matrix("../../../test_data/ww_36_pmec_36.pm");
+
+    MPIL_Finalize();
+    MPI_Finalize();
+    return 0;
+}  // end of main() //
